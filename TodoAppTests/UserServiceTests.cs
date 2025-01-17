e770a1ad1ec8d4344a9fe177fc667c44985e2e30
Lester Botello <lunarian.software@outlook.com>
2025-01-17 18:47:46 -0500

    Add another test for User Service

--- TodoAppTests/UserServiceTests.cs
+++ TodoAppTests/UserServiceTests.cs
@@ -1,13 +1,21 @@
+using AutoFixture;
 using Moq;
 
 [TestFixture]
 public class UserServiceTests
 {
+    Fixture _fixture = new Fixture();
+
     [Test]
     public void User_IfIsValid_ShouldReturnTrue()
     {
         // Arrange
-        var user = new User(1, "admin", "admin", "admin@admin.adm", "Admin", "Admin");
+        var user = _fixture.Build<User>()
+            .With(u => u.Username, "admin")
+            .With(u => u.Password, "admin")
+            .With(u => u.Email, "admin@admin.adm")
+            .Create();
+
         var userRepositoryMock = new Mock<IUserRepository>();
         userRepositoryMock.Setup(userRepository => userRepository.FindByEmail("admin@admin.adm")).Returns(user);
         var sut = new UserService(userRepositoryMock.Object);
@@ -17,5 +25,28 @@ public class UserServiceTests
 
         // Assert
         Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void User_IfIsNotValid_ShouldReturnFalse()
+    {
+        // Arrange
+        var user = _fixture.Build<User>()
+            .With(u => u.Username, "admin")
+            .With(u => u.Password, "admin")
+            .With(u => u.Email, "pepito@escuelota.com")
+            .Create();
+
+        var returnedUser = _fixture.Create<User>();
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock.Setup(userRepository => userRepository.FindByEmail("pepito@escuelota.com")).Returns(returnedUser);
+        var sut = new UserService(userRepositoryMock.Object);
+
+        // Act
+        var result = sut.IsValidUser(user);
+
+        // Assert
+        Assert.That(result, Is.False);
     }
 }
